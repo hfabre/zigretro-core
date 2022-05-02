83a6ed03e11fa52639cad28fda9c72006e57bc52
Hugo Fabre <hfabre@synbioz.com>
2022-05-02 23:43:27 +0200

    add input handling

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,20 @@ const lr = @cImport(@cInclude("libretro.h"));
 const video_width = 200;
 const video_height = 150;
 const bpp = 4;
+const speed = 1;
 const video_pixels = video_height * video_width;
 const allocator = std.heap.c_allocator;
 
-var last_aspect: f32 = 0.0;
-var last_sample_rate: f32 = 0.0;
-
-var logging: lr.retro_log_callback = undefined;
-var log_cb: lr.retro_log_printf_t = undefined;
-var environ_cb: lr.retro_environment_t = undefined;
-var video_cb: lr.retro_video_refresh_t = undefined;
-var audio_cb: lr.retro_audio_sample_t = undefined;
-var audio_batch_cb: lr.retro_audio_sample_batch_t = undefined;
-var input_poll_cb: lr.retro_input_poll_t = undefined;
-var input_state_cb: lr.retro_input_state_t = undefined;
-
 var player_x: i32 = 0;
 var player_y: i32 = 0;
+var key_map = std.AutoHashMap(c_uint, Key).init(allocator);
+
+const Key = enum {
+    up,
+    down,
+    left,
+    right
+};
 
 const Color = struct {
     a: u8,
@@ -44,11 +41,40 @@ const white = Color {
     .b = 255
 };
 
+// Logic
+
+export fn retro_run() void {
+    process_inputs();
+    draw_rectangle(0, 0, video_width, video_height, black);
+    draw_rectangle(player_x, player_y, 20, 20, white);
+    screen_to_frame_buffer();
+    video_cb.?(@ptrCast(*anyopaque, frame_buffer.ptr), video_width, video_height, bpp * video_width * @sizeOf(u8));
+}
+
+fn process_inputs() void {
+    var it = key_map.iterator();
+
+    while (it.next()) |kv| {
+        var pressed = input_state_cb.?(0, lr.RETRO_DEVICE_JOYPAD, 0, kv.key_ptr.*);
+
+        if (pressed != 0) {
+            switch (kv.value_ptr.*) {
+                Key.up => player_y -= speed,
+                Key.down => player_y += speed,
+                Key.right => player_x += speed,
+                Key.left => player_x -= speed
+            }
+        }
+    }
+}
+
+// Utils functions
+
 fn draw_rectangle(x: i32, y: i32, w: i32, h: i32, color: Color) void {
     var i: usize = @intCast(usize, y);
 
     while (i < y + h) {
-        var j: usize = @intCast(usize, y);
+        var j: usize = @intCast(usize, x);
 
         while (j < x + w) {
             screen[i][j] = color;
@@ -79,27 +105,45 @@ fn screen_to_frame_buffer() void {
     }
 }
 
-export fn retro_run() void {
-    draw_rectangle(0, 0, video_width, video_height, black);
-    draw_rectangle(player_x, player_y, 20, 20, white);
-    screen_to_frame_buffer();
-    video_cb.?(@ptrCast(*anyopaque, frame_buffer.ptr), video_width, video_height, bpp * video_width * @sizeOf(u8));
-}
 
 // Init
 
 var frame_buffer: []u8 = undefined;
 var screen: [video_height][video_width]Color = undefined;
+
 export fn retro_init() void {
     frame_buffer = allocator.alloc(u8, video_pixels * bpp) catch {
-        std.log.info("Could not allocate memory", .{});
-        std.os.exit(1);
-        // TODO: environ_cb.?(lr.RETRO_ENVIRONMENT_SHUTDOWN, null)
+        handle_error("Could not allocate memory");
+        // Trick to avoid error: expected type '[]u8', found 'void'
+        return;
     };
+
+    key_map.put(lr.RETRO_DEVICE_ID_JOYPAD_UP, Key.up) catch {
+        handle_error("Could not allocate memory");
+    };
+
+    key_map.put(lr.RETRO_DEVICE_ID_JOYPAD_DOWN, Key.down) catch {
+        handle_error("Could not allocate memory");
+    };
+
+    key_map.put(lr.RETRO_DEVICE_ID_JOYPAD_RIGHT, Key.right) catch {
+        handle_error("Could not allocate memory");
+    };
+
+    key_map.put(lr.RETRO_DEVICE_ID_JOYPAD_LEFT, Key.left) catch {
+        handle_error("Could not allocate memory");
+    };
+}
+
+fn handle_error(message: []const u8) void {
+    std.log.info("{s}", .{message});
+    // TODO: environ_cb.?(lr.RETRO_ENVIRONMENT_SHUTDOWN, null)
+    std.os.exit(1);
 }
 
 export fn retro_deinit() void {
     allocator.free(frame_buffer);
+    key_map.deinit();
 }
 
 export fn retro_set_environment(cb: lr.retro_environment_t) void {
@@ -131,6 +175,15 @@ export fn retro_load_game(_: [*c]lr.retro_game_info) bool {
 
 // Setting up callbacks
 
+var logging: lr.retro_log_callback = undefined;
+var log_cb: lr.retro_log_printf_t = undefined;
+var environ_cb: lr.retro_environment_t = undefined;
+var video_cb: lr.retro_video_refresh_t = undefined;
+var audio_cb: lr.retro_audio_sample_t = undefined;
+var audio_batch_cb: lr.retro_audio_sample_batch_t = undefined;
+var input_poll_cb: lr.retro_input_poll_t = undefined;
+var input_state_cb: lr.retro_input_state_t = undefined;
+
 export fn retro_set_audio_sample(cb: lr.retro_audio_sample_t) void {
     audio_cb = cb;
 }
@@ -161,18 +214,11 @@ export fn retro_get_system_info(info: [*c]lr.struct_retro_system_info) void {
 }
 
 export fn retro_get_system_av_info(info: [*c]lr.retro_system_av_info) void {
-    const aspect = 0.0;
-    const sampling_rate = 30000.0;
-
-
     info.*.geometry.base_width = video_width;
     info.*.geometry.base_height = video_height;
     info.*.geometry.max_width = video_width;
     info.*.geometry.max_height = video_height;
-    info.*.geometry.aspect_ratio = aspect;
-
-    last_aspect = aspect;
-    last_sample_rate = sampling_rate;
+    info.*.geometry.aspect_ratio = 0.0;
 }
 
 // Mandatory symbols
